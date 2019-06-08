7770b5f477f4c24ba64f331cccef546891ac8fbd
minhthangdang <dang.minh.thang@gmail.com>
2019-06-08 13:14:17 -0700

    moved csv files to separate folder

--- myvgg_net_train.py
+++ myvgg_net_train.py
@@ -25,6 +25,7 @@ import pickle
 data, labels, label_binarizer = preprocess.build_data_and_labels()
 
 # split the data into training and testing (80% and 20% respectively)
+print("[INFO] splitting data for train/test...")
 (trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.2, random_state=config.RANDOM_SEED)
 
 # construct the image generator for data augmentation
@@ -61,6 +62,7 @@ f.write(pickle.dumps(label_binarizer))
 f.close()
 
 # print out classification report on test data
+print("[INFO] preparing classification report...")
 predictions = model.predict(testX, batch_size=config.BATCH_SIZE)
 print(classification_report(testY.argmax(axis=1), predictions.argmax(axis=1), target_names=label_binarizer.classes_))
 
